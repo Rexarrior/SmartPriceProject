0c5b32a82a4f4d20c2b6506d50988b61f2d1aa15
KKastaneda <ssnnp@mail.ru>
2020-05-08 01:14:01 +0300

    The minimal version is almost finished. This commit is done in order to save code in case my computer suddenly blacks out instantly and never comes to life again. This is possible.

---
+++ IoTServer/ImagesServer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace IoTServer
+{
+    class ImagesServer
+    {
+        private const long _ServerNotRunning = 0;
+        private const long _ServerRunning = 1;
+
+        private long _IsServerRunning;
+
+        private Thread _ConnectionsAccepterThread;
+        private List<Thread> _RequestHandlerThreads;
+
+        private Socket _ServerSocket;
+        private List<Socket> _RequestHandlerSockets;
+
+        private void Send(Socket socket, byte[] data)
+        {
+            byte[] signal = new byte[1];
+            socket.Send(data);
+            socket.Receive(signal);
+        }
+
+        private void Receive(Socket socket, byte[] buffer)
+        {
+            byte[] signal = new byte[1];
+            socket.Receive(buffer);
+            socket.Send(signal);
+        }
+
+        private void SendImage(string filename)
+        {
+
+        }
+
+        /*
+         * Runs a loop for handling incoming requests.
+         */
+        public void RequestHandler(object socket)
+        {
+            try
+            {
+                string[] filePaths = System.IO.File.ReadAllLines("config.txt");
+
+                MSAccessDBReader iotDBReader = new MSAccessDBReader();
+
+                while (Interlocked.Read(ref _IsServerRunning) == _ServerRunning)
+                {
+                    byte[] buffer = new byte[1024];
+
+                    Receive(socket as Socket, buffer);
+
+                    int price = iotDBReader.GetPrice(filePaths[0], buffer[0]);
+
+                    if (price == -1)
+                    {
+                        // the last item in filePaths is the path to an "error" image
+                        price = filePaths.Length - 2;
+                    }
+                    // +1 'cause at position 0 we've got path to DB
+                    SendImage(filePaths[price + 1]);
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+
+        /*
+         * Binds a server socket to the specified port
+         * and Starts listening asynchronously, exiting
+         * the function. As soon as an someone connects,
+         * creates a thread that begins communicating by
+         * receiving incoming requests and handling them
+         * with HandleRequest function
+         */
+        public bool Start(int port)
+        {
+            if (Interlocked.Read(ref _IsServerRunning) == _ServerRunning)
+            {
+                return false;
+            }
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, port);
+
+            _ServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _ServerSocket.Bind(serverEndPoint);
+            _ServerSocket.Listen(1);
+
+            _ConnectionsAccepterThread = new Thread(delegate ()
+                {
+                    try
+                    {
+                        while (Interlocked.Read(ref _IsServerRunning) == _ServerRunning)
+                        {
+                            Socket newConnectionSocket = _ServerSocket.Accept();
+                            Thread newConnectionThread = new Thread(RequestHandler);
+
+                            _RequestHandlerSockets.Add(newConnectionSocket);
+                            _RequestHandlerThreads.Add(newConnectionThread);
+
+                            newConnectionThread.Start(newConnectionSocket);
+                        }
+                    }
+                    catch(Exception exc)
+                    {
+                        Console.WriteLine(exc.Message);
+                    }
+                }
+            );
+
+            _IsServerRunning = _ServerRunning;
+
+            _ConnectionsAccepterThread.Start();
+
+            return true;
+        }
+
+        public bool Stop(int timeoutBeforeForceTerminationMillisecs = 0)
+        {
+            if (Interlocked.CompareExchange(ref _IsServerRunning, _ServerNotRunning, _ServerRunning) == _ServerNotRunning)
+            {
+                return false;
+            }
+
+            Thread.Sleep(timeoutBeforeForceTerminationMillisecs);
+
+            _ServerSocket.Close();
+            _ConnectionsAccepterThread.Abort();
+
+            for (int i = 0; i < _RequestHandlerSockets.Count; i++)
+            {
+                _RequestHandlerSockets[i].Close();
+                _RequestHandlerThreads[i].Abort();
+            }
+
+            return true;
+        }
+
+        public ImagesServer()
+        {
+            _IsServerRunning = _ServerNotRunning;
+        }
+    }
+}
--- IoTServer/MSAccessDBReader.cs
+++ IoTServer/MSAccessDBReader.cs
@@ -3,26 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.Data.Odbc;
 using Microsoft.SqlServer;
-
+using System.IO;
 
 namespace IoTServer
 {
-    public class MSAccessDBReader : IDBReader
+    public class MSAccessDBReader
     {
-        Access.Application OAcesss = null;
-        
-        public string ReadRecord(string sqlRequest)
+        public int GetPrice(string dbFilepath, int deviceID)
         {
+            if (!File.Exists(dbFilepath))
+            {
+                throw new Exception(dbFilepath + " not found");
+            }
 
+            string connetionString = null;
+            connetionString = @"Driver={Microsoft Access Driver 
+                                (*.mdb, *.accdb)};DBQ=" + dbFilepath;
+            OdbcConnection odbcConnection = new OdbcConnection(connetionString);
 
-            return "";
-        }
+            try
+            {
+                odbcConnection.Open();
 
-        public bool OpenDB(string filepath)
-        {
-            
+                string[] restrictions = new string[4];
+                restrictions[2] = "Products";
+
+                DataTable schema = odbcConnection.GetSchema("Tables");
+
+                int foundPrice = -1;
+
+                foreach (DataRow row in schema.Rows)
+                {
+                    if (Convert.ToInt32(row["device_id"].ToString(), 10) == deviceID)
+                    {
+                        if (foundPrice == -1)
+                        {
+                            foundPrice = Convert.ToInt32(row["device_id"].ToString(), 10);
+                        }
+                        else
+                        {
+                            foundPrice = -1;
+                            break;
+                        }
+                    }
+                }
+                    
+                odbcConnection.Close();
+
+                return foundPrice;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return -1;
+            }
         }
     }
 }
