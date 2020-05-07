0b3d353ef06a10539e220038c2d14a76b5a11ec2
KKastaneda <ssnnp@mail.ru>
2020-05-07 21:38:19 +0300

    Initial Server branch commit

---
+++ IoTServer/MSAccessDBReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+using Microsoft.SqlServer;
+
+
+namespace IoTServer
+{
+    public class MSAccessDBReader : IDBReader
+    {
+        Access.Application OAcesss = null;
+        
+        public string ReadRecord(string sqlRequest)
+        {
+
+
+            return "";
+        }
+
+        public bool OpenDB(string filepath)
+        {
+            
+        }
+    }
+}
---
+++ IoTServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoTServer
+{
+    public class Test
+    {
+        public void Huy()
+        {
+            Console.WriteLine("Huy");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test tt = new Test();
+            tt.Huy();
+        }
+    }
+}
